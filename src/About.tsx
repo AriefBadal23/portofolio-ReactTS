13b9d8988d1774950aae24226abd656d0e64cd5a
Arief Badal <ariefbadal21@outlook.com>
2025-03-28 11:12:33 +0100

    Change about me text

--- src/About.tsx
+++ src/About.tsx
@@ -4,25 +4,17 @@ export default class About extends Component {
   render() {
     return (
       <div className=" mt-10 px-10 " id="about me">
-        <h3 className="bg-[#0f172a] sticky top-0 md:ml-150 md: text-2xl">
+        <h3 className="bg-[#0f172a] sticky top-0 text-2xl md:ml-150 ">
           About Me
         </h3>
         <p className="font-light md:text-left md:ml-150 text-l ">
-          I am a second-year Computer Science student with experience working on
-          various projects, both in teams and individually. I have worked with
-          C# for the backend, Python and ReactJS for the frontend, and use tools
-          like Docker and PostgreSQL.
-        </p>
-        <br></br>
-        <p className="font-light md:text-left md:ml-150 text-l ">
-          My passion lies primarily in backend development, where I build
-          WebAPIs and ensure that data flows correctly and securely to the
-          frontend. I am also interested in cloud development and building
-          scalable applications with Cloud Providers. As an aspiring backend
-          developer, I aim to use my creativity and knowledge to develop secure,
-          scalable, and maintainable solutions. I continuously strive to improve
-          and learn, with the goal of helping companies build robust software
-          that is future-proof.
+          I’m a second-year Computer Science student experienced in both team
+          and individual projects. I specialize in backend development using C#,
+          building secure WebAPIs, and ensuring efficient data flow to the
+          frontend. I also work with Python, ReactJS, Docker, and PostgreSQL,
+          and have a keen interest in cloud development and scalable
+          applications. I strive to create secure, scalable, and maintainable
+          solutions while continuously expanding my skills.
         </p>
       </div>
     );
