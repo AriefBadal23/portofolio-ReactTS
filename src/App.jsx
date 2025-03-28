10031dbb8fb3cc84fdc43cefce69db3257b2908b
Arief Badal <ariefbadal21@outlook.com>
2025-03-28 11:13:05 +0100

    Add projects image and add CTA button

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ import About from "./About";
 import { useEffect } from 'react';
 
 import  ProjectContainer  from "./ProjectContainer";
+import Skills from './Skills';
 function App() {
 
   let projects = [
-    {  title: "Associate Degree graduation project | Image Service microservice", url: "https://www.google.com", description: "Created an webservice (API) to resize images in different ratios for the hello-energy platform, which also increased the loading performance. For this I have used Flask for the API, made use of Docker, Postman and wrote unittest with Pytest.", tools: ["Python", "C#"] , image: "imageservice.png"},
+    {  title: "Image Service", url: "https://www.google.com", description: "Created an webservice (API) to resize images in different ratios for the hello-energy platform, which also increased the loading performance. For this I have used Flask for the API, made use of Docker, Postman and wrote unittest with Pytest.", tools: ["Python", "C#"] , image: "imageservice.png"},
     { title: "Album API", url: "https://www.google.com", description: "Album API backend created with ASP.NET Core, Entity Framework Core and ReactJS for the frontend. Made use of Docker to containarize the application and deployed it on AWS.", tools: ["ASP.NET Core", "C#"] , image: "albumapi.png"},
-    { title: "TODO API with Blazor UI", url: "https://www.google.com", description: "ASP.NET Core API with session based authentication and frontend created with Blazor", tools: ["ASP.NET Core", "C#", "Blazor"] }]
+    { title: "TODO API with Blazor UI", url: "https://www.google.com", description: "ASP.NET Core API with session based authentication and frontend created with Blazor", tools: ["ASP.NET Core", "C#", "Blazor"], image:"image.png" }]
     // { title: "Image Service", url: "https://www.google.com", description: "Image service description", tools: ["Python", "C#"] }]
   useEffect(() => {
     document.documentElement.classList.add('dark');
@@ -23,6 +24,7 @@ function App() {
       <About />
       <Navigation/>
       <ProjectContainer projects={projects} />
+      <Skills/>
     </div>
 
   )
